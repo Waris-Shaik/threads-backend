9d6cd5c290b661b13f758b86d75e948ce4cdd539
waris <shaikwaris61@gmail.com>
2024-01-16 11:49:44 +0530

    modified minor change

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ import connectDB from "./database/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.js";
 import postRouter from "./routes/post.js";
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 import cors from "cors";
 
-const app = express(); 
- 
+const app = express();
+
 // .lock_files 🤐;
 dotenv.config();
 const PORT = process.env.PORT || 8081;
@@ -18,35 +18,40 @@ connectDB();
 
 // cloud__connection ⛅⛅;
 cloudinary.config({
-  cloud_name:process.env.CLOUD_NAME,
-  api_key:process.env.CLOUD_API_KEY,
-  api_secret:process.env.CLOUD_API_SECRET_KEY
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET_KEY,
 });
 
-
 // builtin__middlewares 🔐;
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  origin: [process.env.FRONYEND_URL],
-  credentials:true,
-}));
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    origin: [process.env.FRONYEND_URL],
+    credentials: true,
+  })
+);
 
-// routes 📡; 
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/posts", postRouter); 
+// routes 📡;
+app.use("/api/users", userRouter);
+app.use("/api/posts", postRouter);
 
 // initaik route
-app.get('/', (req, res)=>{
+app.get("/", (req, res) => {
   res.status(200).json({
-    success:true,
+    success: true,
     message: "Welcome to threads-root API",
-  })
-})
+  });
+});
 
 // SERVER__PORT 🖥️👽;
 app.listen(PORT, () => {
-  console.log(`server is listening on port:${PORT} 🚀 in ${process.env.NODE_ENV === "Development" ? "⌨️" : "🛢️"} ${process.env.NODE_ENV} mode.`);
+  console.log(
+    `server is listening on port:${PORT} 🚀 in ${
+      process.env.NODE_ENV === "Development" ? "⌨️" : "🛢️"
+    } ${process.env.NODE_ENV} mode.`
+  );
 });
